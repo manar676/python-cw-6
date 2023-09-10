62d60745bdc387f31769a2f6bbf13bffe2f6b26f
manar676 <142670547+manar676@users.noreply.github.com>
2023-09-10 03:22:50 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # write your code here
+person ={
+    "name":"manar" ,
+    "age":17 ,
+    "hobbies":['basketball','shooting','badminton']
+}
+print(person.get["name"])
+print(person.get["age"])
+
+print(person["name"])
+print(person["age"])
+
+person["age"]="20"
+person["country"]="kuwait"
+
+print(f"my name is{person['name']},I am from{person['country']}and I am {person['age']} years old,I really like playing {person['hobbies']} ")
+print(f"{person}")
+ 
+person.get("bowling")
+def check_hobbies(person):
+    if person["hobbies"]>=3:
+        print("WOW YOU ARE AMAZING")
+
+
+
